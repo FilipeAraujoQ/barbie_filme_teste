28b4dc6e643488d599568e0b09fddbb23c737de6
Filipe Araujo Queiroz <146143459+FilipeAraujoQ@users.noreply.github.com>
2023-11-23 19:21:01 -0300

    projeto final

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 8,
+    spaceBetween: 10,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
